0f9d6c57a84a84c2ed653ba0b25d9839efb0a990
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-23 19:23:47 -0500

    Refactor with capital letter

--- 4_queries/3_assistance_req_data.sql
+++ 4_queries/3_assistance_req_data.sql
@@ -1,4 +1,4 @@
-SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (completed_at-started_at) AS duration
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
