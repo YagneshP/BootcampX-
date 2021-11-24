ed29b9b214b4f9c085f8f363e18c132cc46829fd
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-23 20:01:21 -0500

    Add query to get cohort with longest assistance req

---
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name , AVG(completed_at - started_at) AS average_assistance_time 
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
