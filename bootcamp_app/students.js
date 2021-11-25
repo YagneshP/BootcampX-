c0da98711f91eeff895409ebd079a42f094f3600
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-25 09:39:05 -0500

    Add Parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,14 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 const limit = process.argv[3];
+const values = [`%${cohort}%`, limit]
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`,values )
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortArg = process.argv[2];
+const values =[`%${process.argv[2]}%`];
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher , cohorts.name AS cohort 
 FROM teachers 
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id 
-WHERE cohorts.name LIKE '%${cohortArg}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} :  ${user.teacher} `);
